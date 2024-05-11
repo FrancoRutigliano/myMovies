2ceb512a8734f8c16239109d22327d44178a9445
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-11 17:30:53 -0300

    creating movie model

---
+++ internal/models/movie.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Movie struct {
+	ID        int64
+	CreatedAt time.Time
+	Title     string
+	Year      int32
+	Runtime   int32
+	Genres    []string
+	Version   int32
+}
