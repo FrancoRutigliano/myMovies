12cd58cdd9a4b37996c013445cdaa13af4d1c695
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-11 17:01:56 -0300

    refactorization of the errors

--- pkg/helpers/message.go
+++ pkg/helpers/message.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log/slog"
 	"net/http"
 	"strconv"
 )
@@ -18,40 +17,22 @@ func (e APIError) Error() string {
 	return fmt.Sprintf("api error: %d", e.StatusCode)
 }
 
-func NewAPIError(statusCode int, err error) APIError {
+func NewAPIError(statusCode int, message string) APIError {
 	return APIError{
 		StatusCode: statusCode,
-		Msg:        err.Error(),
+		Msg:        message,
 	}
+}
+
+func SendCustom(w http.ResponseWriter, statusCode int, message string) {
+	apiErr := NewAPIError(statusCode, message)
+	WriteJson(w, statusCode, apiErr)
 }
 
 func InvalidRequestData(errors map[string]string) APIError {
 	return APIError{
 		StatusCode: http.StatusUnprocessableEntity,
 		Msg:        errors,
-	}
-}
-
-func InvalidJSON() APIError {
-	return NewAPIError(http.StatusBadRequest, fmt.Errorf("invalid json request data"))
-}
-
-type APIFunc func(w http.ResponseWriter, r *http.Request) error
-
-func make(h APIFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := h(w, r); err != nil {
-			if apiErr, ok := err.(APIError); ok {
-				WriteJson(w, apiErr.StatusCode, apiErr)
-			} else {
-				errResp := map[string]any{
-					"statuscode": http.StatusInternalServerError,
-					"msg":        "internal server error",
-				}
-				WriteJson(w, http.StatusInternalServerError, errResp)
-			}
-			slog.Error("HTTP API error", "err", err.Error(), "path", r.URL.Path)
-		}
 	}
 }
 
