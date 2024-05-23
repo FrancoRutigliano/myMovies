e17aee767faf0aa146efc4029e2a4f6125cdc49a
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-23 17:31:14 -0300

    Fix metodo de compartir la funcion

--- pkg/helpers/password.go
+++ pkg/helpers/password.go
@@ -2,7 +2,7 @@ package helpers
 
 import "golang.org/x/crypto/bcrypt"
 
-func hashPassword(password string) (string, error) {
+func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
