b012055c98ccccc8ccc3a2714b588bc395b86d49
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-23 17:55:01 -0300

    registro de usuarios terminado

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -2,10 +2,10 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/FrancoRutigliano/myMovies/internal/models"
 	"github.com/FrancoRutigliano/myMovies/pkg/helpers"
-	utils "github.com/FrancoRutigliano/myMovies/pkg/helpers"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -27,21 +27,21 @@ func (auth *AuthHandler) RegisterRoutes(router *http.ServeMux) {
 func (auth *AuthHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	var payload models.UserRegister
 
-	if err := utils.ParseJson(r, &payload); err != nil {
-		utils.SendCustom(w, http.StatusBadRequest, err.Error())
+	if err := helpers.ParseJson(r, &payload); err != nil {
+		helpers.SendCustom(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if err := utils.Validate.Struct(&payload); err != nil {
+	if err := helpers.Validate.Struct(&payload); err != nil {
 		_ = err.(validator.ValidationErrors)
-		utils.SendCustom(w, http.StatusUnprocessableEntity, err.Error())
+		helpers.SendCustom(w, http.StatusUnprocessableEntity, err.Error())
 		return
 	}
 
 	// TODO: Si el usuario existe por el email, retornar un error
 	err := auth.store.EmailExist(payload.Email)
 	if err != nil {
-		utils.SendCustom(w, http.StatusBadRequest, err.Error())
+		helpers.SendCustom(w, http.StatusBadRequest, err.Error())
 	}
 
 	// TODO: hash el password
@@ -50,6 +50,19 @@ func (auth *AuthHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
 		helpers.SendCustom(w, http.StatusInternalServerError, err.Error())
 	}
 
+	// TODO: Crear el usuario
+	auth.store.CreateUser(&models.User{
+		Name:      payload.Name,
+		Email:     payload.Email,
+		Password:  hashPassword,
+		Role:      "user",
+		CreatedAt: time.Now().Format(time.RFC3339),
+	})
+	if err != nil {
+		helpers.SendCustom(w, http.StatusInternalServerError, err.Error())
+	}
+
+	helpers.WriteJson(w, http.StatusCreated, payload, "user")
 }
 
 func (auth *AuthHandler) LoginUser(w http.ResponseWriter, r *http.Request) {
