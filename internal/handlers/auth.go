2d200d81378c5e3cc1412ed8738be80c1d368ad6
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-23 17:34:49 -0300

    password hasheada correctamente

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/FrancoRutigliano/myMovies/internal/models"
+	"github.com/FrancoRutigliano/myMovies/pkg/helpers"
 	utils "github.com/FrancoRutigliano/myMovies/pkg/helpers"
 	"github.com/go-playground/validator/v10"
 )
@@ -42,6 +43,13 @@ func (auth *AuthHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		utils.SendCustom(w, http.StatusBadRequest, err.Error())
 	}
+
+	// TODO: hash el password
+	hashPassword, err := helpers.HashPassword(payload.Password)
+	if err != nil {
+		helpers.SendCustom(w, http.StatusInternalServerError, err.Error())
+	}
+
 }
 
 func (auth *AuthHandler) LoginUser(w http.ResponseWriter, r *http.Request) {
