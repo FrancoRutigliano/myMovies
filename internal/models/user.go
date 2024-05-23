106220254ba4c17ccc23d6110ca07a424aa298fc
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-23 11:03:46 -0300

    definida estructura user && userMovie

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,15 @@
 package models
+
+type User struct {
+	ID        int64       `json:"id"`
+	Email     string      `json:"email"`
+	Password  string      `json:"password"`
+	Movies    []UserMovie `json:"movies"`
+	Role      string      `json:"role"`
+	CreatedAt string      `json:"created_at"`
+}
+
+type UserMovie struct {
+	MovieID        int64 `json:"movie_id"`
+	MovieTheaterID int64 `json:"movie_theater_id"`
+}
