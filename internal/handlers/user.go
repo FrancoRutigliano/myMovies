b3aa2119526056cfb16a157895af02e03c045a37
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-26 23:16:52 -0300

    registro de rutas para usuarios

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,9 +1,28 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
 
-type UserHandler struct{}
+	"github.com/FrancoRutigliano/myMovies/internal/models"
+	"github.com/FrancoRutigliano/myMovies/pkg/helpers"
+)
+
+type UserHandler struct {
+	store models.Users
+}
+
+func NewUserHandler(store models.Users) *UserHandler {
+	return &UserHandler{
+		store: store,
+	}
+}
 
 func (u *UserHandler) RegisterRoutes(router *http.ServeMux) {
+	router.HandleFunc("GET /users", u.GetAllUsers)
+}
 
+func (u *UserHandler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	users := u.store.GetAll()
+
+	helpers.WriteJson(w, http.StatusOK, users, "users")
 }
