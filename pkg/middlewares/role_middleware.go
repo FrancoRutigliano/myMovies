bea33a7d0b59a98b4546a24544e4a3574befd230
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-30 15:10:54 -0300

    middleware de autenticacion de roles

--- pkg/middlewares/role_middleware.go
+++ pkg/middlewares/role_middleware.go
@@ -1,1 +1,44 @@
 package middlewares
+
+import (
+	"net/http"
+
+	"github.com/FrancoRutigliano/myMovies/pkg/helpers"
+	authHelpers "github.com/FrancoRutigliano/myMovies/pkg/helpers/auth"
+	"github.com/golang-jwt/jwt"
+)
+
+func RoleMiddleware(expRole string) func(http.Handler) http.HandlerFunc {
+	return func(next http.Handler) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			tokenStr := authHelpers.GetTokenFromRequest(r)
+			if tokenStr == "" {
+				helpers.SendCustom(w, http.StatusForbidden, "Unauthorized")
+				return
+			}
+
+			// validar la signature del token para saber que el token no ha sido alterado
+			t, err := authHelpers.ValidateToken(tokenStr)
+			if err != nil {
+				helpers.SendCustom(w, http.StatusForbidden, "Unauthorized")
+				return
+			}
+
+			claims, ok := t.Claims.(jwt.MapClaims)
+			if !ok || !t.Valid {
+				helpers.SendCustom(w, http.StatusForbidden, "Unauthorized")
+				return
+			}
+
+			role := claims["userRole"].(string)
+			// si el rol no coincide
+			if role != expRole {
+				helpers.SendCustom(w, http.StatusForbidden, "Unauthorized")
+				return
+			}
+
+			// pasar el request al manejador siguiente
+			next.ServeHTTP(w, r)
+		}
+	}
+}
