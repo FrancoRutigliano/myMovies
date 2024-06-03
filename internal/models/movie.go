b011c36799b83a81ce62089a9b1960b0dcc0681a
FrancoRutigliano <francorutigliano11@gmail.com>
2024-06-03 20:42:20 -0300

    añdido validaciones al payload de movies

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -3,11 +3,10 @@ package models
 import "time"
 
 type Movie struct {
-	ID        int64     `json:"id"`
+	ID        int64     `json:"id,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
-	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty"`
-	Genres    []string  `json:"genres,omitempty"`
-	Version   int32     `json:"version"`
+	Title     string    `json:"title" validate:"required,min=3,max=50"`
+	Year      int32     `json:"year,omitempty" validate:"required,min=1900,max=2100"`
+	Runtime   int32     `json:"runtime,omitempty" validate:"required,min=50"`
+	Genres    []string  `json:"genres,omitempty" validate:"required,min=1,max=3"`
 }
