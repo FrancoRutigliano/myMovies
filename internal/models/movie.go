36f9a6d5f608eb6e9e935ad83aa0e970ae9d5046
FrancoRutigliano <francorutigliano11@gmail.com>
2024-06-11 11:50:43 -0300

    modularizacion de modelos y creacion de review

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -14,5 +14,6 @@ type Movie struct {
 	Title     string   `json:"title" validate:"required,min=3,max=50"`
 	Year      int32    `json:"year,omitempty" validate:"required,min=1900,max=2100"`
 	Runtime   int32    `json:"runtime,omitempty" validate:"required,min=50"`
+	Review    []Review `json:"reviews,omitempty"`
 	Genres    []string `json:"genres,omitempty" validate:"required,min=1,max=3"`
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -7,18 +7,11 @@ type Users interface {
 }
 
 type User struct {
-	ID        int64        `json:"id,omitempty"`
-	Name      string       `json:"name" validate:"required,min=3,max=50"`
-	Email     string       `json:"email,omitempty" validate:"required,email"`
-	Password  string       `json:"password,omitempty" validate:"required,min=6"`
-	Role      string       `json:"role,omitempty"`
-	Review    []UserReview `json:"review"`
-	CreatedAt string       `json:"created_at,omitempty"`
-}
-
-type UserReview struct {
-	MovieID        int64  `json:"movie_id"`
-	UserID         int64  `json:"user_id"`
-	Review         string `json:"review"`
-	ReviewDatetime string `json:"review_datetime"`
+	ID        int64    `json:"id,omitempty"`
+	Name      string   `json:"name" validate:"required,min=3,max=50"`
+	Email     string   `json:"email,omitempty" validate:"required,email"`
+	Password  string   `json:"password,omitempty" validate:"required,min=6"`
+	Role      string   `json:"role,omitempty"`
+	Review    []Review `json:"review"`
+	CreatedAt string   `json:"created_at,omitempty"`
 }
